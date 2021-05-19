6d728d4b2642d8189bac196c76002b51cf7f32be
sago35 <sago35@gmail.com>
2021-05-19 22:13:32 +0900

    Change UART / sercom

--- examples/01_check_firmware_version/wioterminal.go
+++ examples/01_check_firmware_version/wioterminal.go
@@ -3,27 +3,44 @@
 package main
 
 import (
+	"device/sam"
 	"machine"
+	"runtime/interrupt"
 	"time"
 
 	"github.com/sago35/go-erpcgen/rtl8720dn"
 )
 
+var (
+	uart *machine.UART
+)
+
+func handleInterrupt(interrupt.Interrupt) {
+	// should reset IRQ
+	uart.Receive(byte((uart.Bus.DATA.Get() & 0xFF)))
+	uart.Bus.INTFLAG.SetBits(sam.SERCOM_USART_INT_INTFLAG_RXC)
+}
+
 func setupRTL8720DN() (*rtl8720dn.RTL8720DN, error) {
 	debug := true
 
-	waitSerial()
+	uart = &machine.UART{
+		Buffer: machine.NewRingBuffer(),
+		Bus:    sam.SERCOM0_USART_INT,
+		SERCOM: 0,
+	}
 
-	uart2 := machine.UART2
-	uart2.Configure(machine.UARTConfig{TX: machine.UART2_TX_PIN, RX: machine.UART2_RX_PIN, BaudRate: 614400})
+	uart.Interrupt = interrupt.New(sam.IRQ_SERCOM0_2, handleInterrupt)
+	uart.Configure(machine.UARTConfig{TX: machine.PB24, RX: machine.PC24, BaudRate: 614400})
 
 	machine.RTL8720D_CHIP_PU.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	machine.RTL8720D_CHIP_PU.Low()
 	time.Sleep(100 * time.Millisecond)
 	machine.RTL8720D_CHIP_PU.High()
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(1000 * time.Millisecond)
+	waitSerial()
 
-	rtl := rtl8720dn.New(uart2)
+	rtl := rtl8720dn.New(uart)
 	rtl.Debug(debug)
 
 	_, err := rtl.Rpc_tcpip_adapter_init()
